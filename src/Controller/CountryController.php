10da56d09b77397e024d182b94535ba941c2d697
Adrien Thouvenin <adaura51@gmail.com>
2020-12-09 11:41:48 +0100

    fix : create function browse country

--- src/Controller/CountryController.php
+++ src/Controller/CountryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\CountryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +12,12 @@ class CountryController extends AbstractController
     /**
      * @Route("/country", name="country")
      */
-    public function index(): Response
+    public function index( CountryRepository $countryRepository): Response
     {
+        $countries = $countryRepository->findAll();
+
         return $this->render('country/index.html.twig', [
-            'controller_name' => 'CountryController',
+            'countries' => $countries
         ]);
     }
 }
